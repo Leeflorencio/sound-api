e3b4939df912a98fc3ee0456c7ad5d20c9a584a8
Leeflorencio <florenciolee63@gmail.com>
2024-06-10 22:17:21 -0300

    retirando imports

--- src/main/java/br/com/sound/repository/ArtistaRepository.java
+++ src/main/java/br/com/sound/repository/ArtistaRepository.java
@@ -1,17 +1,12 @@
 package br.com.sound.repository;
 
-import br.com.sound.dto.ArtistaDto;
 import br.com.sound.model.ArtistaModel;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface ArtistaRepository extends JpaRepository<ArtistaModel, Long> {
@@ -19,4 +14,5 @@ public interface ArtistaRepository extends JpaRepository<ArtistaModel, Long> {
 
     @Query(value = "SELECT * FROM artistas WHERE nome LIKE %:nome%", nativeQuery = true)
     List<ArtistaModel> findAllByNome(@Param("nome") String nome);
+
 }
